1600e109a964c203f358e7d7a455c9bfc5386e99
Jordan Patapoff <jordan@clyp.it>
2016-12-10 00:30:36 -0600

    Number Genie test

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-var http = require('http')
- var port = process.env.PORT || 8080;
- console.log("conSUL test");
- http.createServer(function(req, res) {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   res.end('Yessika is awesome!\n');
- }).listen(port);
+'use strict';
+
+// Enable actions client library debugging
+process.env.DEBUG = 'actions-on-google:*';
+
+let Assistant = require('actions-on-google');
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.json({type: 'application/json'}));
+
+const GENERATE_ANSWER_ACTION = 'generate_answer';
+const CHECK_GUESS_ACTION = 'check_guess';
+
+app.post('/', function (request, response) {
+  console.log('headers: ' + JSON.stringify(request.headers));
+  console.log('body: ' + JSON.stringify(request.body));
+
+  const assistant = new Assistant({request: request, response: response});
+  response.sendStatus(200); // OK
+});
+
+// Start the server
+var server = app.listen(app.get('port'), function () {
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit.');
+});
