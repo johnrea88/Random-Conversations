0890a38b68ddb492c57e7c79e35fd3aefa36645e
Jordan Patapoff <jordan@clyp.it>
2016-12-10 16:07:02 -0600

    more prototyping

--- server.js
+++ server.js
@@ -11,44 +11,29 @@ let app = express();
 app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json({type: 'application/json'}));
 
-const GENERATE_ANSWER_ACTION = 'generate_answer';
-const CHECK_GUESS_ACTION = 'check_guess';
 const START_NEW_CHAT_ACTION = 'start_new_chat';
+const CHECK_FOR_MESSAGES_ACTION = 'check_for_messages';
+const REPLY_TO_MESSAGE_ACTION = 'reply_to_message';
+const END_CONVERSATION_ACTION = 'end_conversation';
+const READ_BACK_CHAT_HISTORY_ACTION = 'read_back_chat_history';
+
+
+app.get('/', function (request, response){
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
 
 app.post('/', function (request, response) {
   console.log('headers: ' + JSON.stringify(request.headers));
   console.log('body: ' + JSON.stringify(request.body));
 
   const assistant = new Assistant({request: request, response: response});
+  let actionMap = new Map();
+  actionMap.set(START_NEW_CHAT_ACTION, startNewChat);
+  assistant.handleRequest(actionMap);
 
-  function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function generateAnswer(assistant) {
-    console.log('generateAnswer');
-    var answer = getRandomNumber(0, 100);
-    assistant.data.answer = answer;
-    assistant.ask('I\'m thinking of a number from 0 and 100. What\'s your first guess?');
-  }
-
- function checkGuess(assistant) {
-      console.log("BAR TEST: " + assistant.data.foo);
-      assistant.data.foo = "bar";
-      console.log('checkGuess');
-      console.log('getRawInput(): ' + assistant.getRawInput());
-      let answer = assistant.data.answer;
-      let guess = parseInt(assistant.getArgument('guess'));
-      if (answer > guess) {
-       assistant.ask('It\'s higher than ' + guess + '. What\'s your next guess?');
-      } else if (answer < guess) {
-       assistant.ask('It\'s lower than ' + guess + '. Next guess?');
-      } else {
-        assistant.tell('Congratulations, that\'s it! I was thinking of ' + answer);
-      }
-  }
-
-  function startNewChat(assistant){
+  function startNewChat(assistant) {
     console.log('startNewChat');
     let message = assistant.getArgument('message');
     let name = assistant.getArgument('name');
@@ -57,16 +42,9 @@ function generateAnswer(assistant) {
     console.log('getRawInput: ' + assistant.getRawInput());
     assistant.tell('Your chat is being setup. Check back in a few minutes for new messages');
   }
-
-  let actionMap = new Map();
-  actionMap.set(GENERATE_ANSWER_ACTION, generateAnswer);
-  actionMap.set(CHECK_GUESS_ACTION, checkGuess);
-  actionMap.set(START_NEW_CHAT_ACTION, startNewChat);
-
-  assistant.handleRequest(actionMap);
 });
 
-// Start the server
+
 var server = app.listen(app.get('port'), function () {
   console.log('App listening on port %s', server.address().port);
   console.log('Press Ctrl+C to quit.');
