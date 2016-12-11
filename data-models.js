3768084faf3044b2138d9833c0cdf49ec2efc5be
Jordan Patapoff <jordan@clyp.it>
2016-12-10 20:46:44 -0600

    made progress w data models

--- data-models.js
+++ data-models.js
@@ -28,9 +28,22 @@ var Conversation = function(initiatingUserId) {
   self.id = lastConversationId;
   self.dateCreated = new Date();
   self.lastUpdated = new Date();
-  self.status = "ACTIVE";
+  self.status = 'ACTIVE';
   self.initiatingUserId = initiatingUserId;
   self.respondingUserId = null;
+
+  self.endConversation = function() {
+    self.status = 'ENDED';
+  };
+
+  self.isActive = function() {
+    return self.status === 'ACTIVE';
+  };
+
+  self.isBetweenUsers = function(userId1, userId2) {
+    return (self.initiatingUserId === userId1 && self.respondingUserId === userId2) ||
+      (self.initiatingUserId === userId2 && self.respondingUserId === userId1);
+  }
 };
 
 var Message = function(text, authoringUserId, conversationId) {
@@ -68,13 +81,23 @@ var DataContainer = function() {
     return user;
   };
 
-  self.getUnmatchedConversation = function(userId) {
+  self.activeConversationExists = function(userId1, userId2) {
     self.conversations.forEach(function(conversation) {
-      if(conversation.respondingUserId === null && conversation.initiatingUserId !== user) {
-        return conversation;
+      if(conversation.isBetweenUsers(userId1, userId2) && conversation.isActive()) {
+        return true;
       }
     });
-    return null;
+    return false;
+  };
+
+  self.getUnmatchedConversations = function(userId) {
+    let conversations = [];
+    self.conversations.forEach(function(conversation) {
+      if(conversation.respondingUserId === null && conversation.initiatingUserId !== user) {
+        conversations.push(conversation);
+      }
+    });
+    return conversations;
   };
 
   // List is ordered oldest to newest
@@ -88,16 +111,46 @@ var DataContainer = function() {
     return foundMessages;
   };
 
-  self.getAllMessagesByMessageId = function(messageId) {
+  self.getConversationIdByMessageId = function(messageId) {
+    self.messages.forEach(function(message) {
+      if(message.id === messageId) return message.conversationId;
+    });
+    return null;
+  };
 
+  self.getAllMessagesByMessageId = function(messageId) {
+    let messages = [];
+    let conversationId = self.getConversationIdByMessageId(messageId);
+    if (!conversationId) return messages;
+    return self.getAllMessagesByConversationId(conversationId);
   };
 
   self.endConversation = function(messageId) {
+    let conversation = self.getConversationByMessageId(messageId);
+    conversation.endConversation();
+  };
 
+  self.startNewConversation = function(name, text, googleUserId) {
+    let user = self.getOrStoreUser(googleUserId, name);
+    let unmatchedConversations = self.getUnmatchedConversations(user.id);
+    unmatchedConversations.forEach(function(unmatchedConversation) {
+      if(!self.activeConversationExists(user.id, unmatchedConversation.initiatingUserId)) {
+        unmatchedConversation.respondingUserId = user.id;
+        let message = new Message(text, user.id, unmatchedConversation.id);
+        self.messages.push(message);
+        return;
+      }
+    });
+    let conversation = new Conversation(user.id);
+    self.conversations.push(conversation);
+    let message = new Message(text, user.id, conversation.id);
+    self.messages.push(message);
   };
 
   self.replyToMessage = function(replyingUserId, replyText, messageIdToReplyTo) {
-
+    let conversationId = self.getConversationIdByMessageId(messageId);
+    let message = new Message(replyText, replyingUserId, conversationId);
+    self.messages.push(message);
   };
 
   self.getUserByUserId = function(userId) {
@@ -107,11 +160,23 @@ var DataContainer = function() {
     return null;
   };
 
+  self.getConversationByConversationId = function(conversationId) {
+    self.conversations.forEach(function(conversation) {
+      if(conversation.id === conversations) return conversation;
+    });
+    return null;
+  };
+
+  self.getConversationByMessageId = function(messageId) {
+    let conversationId = self.getConversationIdByMessageId(messageId);
+    return self.getConversationByConversationId(conversationId);
+  };
+
   self.getConversationsByUserId = function(userId) {
     let conversations = [];
     self.conversations.forEach(function(conversation) {
       if(conversation.initiatingUserId === userId || conversation.respondingUserId === userId) {
-        conversations.push(conversation);
+        if(conversation.isActive()) conversations.push(conversation);
       }
     });
     return conversations;
@@ -121,6 +186,7 @@ var DataContainer = function() {
     let unreadMessages = [];
     let allConversations = self.getConversationsByUserId(userId);
     allConversations.forEach(function(conversation) {
+      if (!conversation.isActive()) continue;
       let allMessages = self.getAllMessagesByConversationId(conversation.id);
       allMessages.forEach(function(message) {
         if(message.isUnread()) unreadMessages.push(message);
